c604cbabc8357d10b7de6c76e10e3fcbb591232d
Christopher-Steel <christopher.g.steel@gmail.com>
2024-05-31 12:18:51 +0100

    lint

--- winrate_tracker_api/app/routes.py
+++ winrate_tracker_api/app/routes.py
@@ -1,16 +1,20 @@
+from flask import jsonify, request
+
 from app import app
+
 from .extensions import db
 from .models import User
-from flask import request, jsonify
 
-@app.route('/api/hello', methods=['GET'])
+
+@app.route("/api/hello", methods=["GET"])
 def hello_api():
-    return {'message': "Hey man"}
+    return {"message": "Hey man"}
 
-@app.route('/api/users', methods=['POST'])
+
+@app.route("/api/users", methods=["POST"])
 def create_user():
     data = request.get_json()
-    new_user = User(username=data['username'], email=data['email'])
+    new_user = User(username=data["username"], email=data["email"])
     db.session.add(new_user)
     db.session.commit()
-    return jsonify({'id': new_user.id, 'username': new_user.username, 'email': new_user.email}), 201
+    return jsonify({"id": new_user.id, "username": new_user.username, "email": new_user.email}), 201
